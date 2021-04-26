e709f77cc9b057cb56eb0c8cff154626ea3a91d2
Syed Mohammad Quadri <55687431+SMQuadri@users.noreply.github.com>
2021-04-26 16:15:44 +0530

    Update mailer.py

--- Store_Entry_Exit_Analysis/mylib/mailer.py
+++ Store_Entry_Exit_Analysis/mylib/mailer.py
@@ -4,8 +4,19 @@ class Mailer:
     
     def __init__(self):
         
-        self.EMAIL = "smquadri1999@gmail.com"
-        self.PASS = "cfytwtrlixiipwrn"
+        # Enter your email below. This email will be used to send alerts.
+        # Eg: "email@gmail.com"
+        self.EMAIL = ""
+        
+        # Enter the email password below. Note that the corresponding mail id you enter above must be a 2 step verified account.
+        # Only then you could generate the corresponding 16 character app key password which you.
+        # Paste your generated 16 character password below.
+        # Eg: "cjudtykamnxdqiow"
+        # To create 2 step verification and then turn it on. You can refer the links below and create an application specific password.
+        # Google mail has a guide here: https://myaccount.google.com/lesssecureapps
+        # For 2 step verified accounts: https://support.google.com/accounts/answer/185833
+        self.PASS = ""
+        
         self.PORT = 465
         self.server = smtplib.SMTP_SSL('smtp.gmail.com', self.PORT)
 
