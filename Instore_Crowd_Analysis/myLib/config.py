ebb8c5ff5932302023edd5645f211d70acf57094
Syed Mohammad Quadri <55687431+SMQuadri@users.noreply.github.com>
2021-04-26 16:03:23 +0530

    Add files via upload

---
+++ Instore_Crowd_Analysis/myLib/config.py
@@ -0,0 +1,28 @@
+#=================================================================
+#                     MANUAL USER CONFIG SETUP
+#=================================================================
+
+# base path to YOLO directory
+MODEL_PATH = "model"
+
+# initialize minimum probability to filter weak detections along with the threshold when applying non-maxima suppression
+MIN_CONF = 0.3
+NMS_THRESH = 0.3
+
+# boolean indicating if NVIDIA CUDA GPU should be used
+USE_GPU = False
+
+# define the minimum safe distance (in pixels) that two people can be from each other
+MIN_DISTANCE = 70
+
+
+#Email alert
+MAIL = 'whitefalconsmq@gmail.com'
+ALERT = True
+Threshold = 17
+
+# Auto run/Schedule the software to run at your desired time
+Scheduler = False
+
+# Auto stop the software after certain a time/hours
+Timer = True
---
+++ Instore_Crowd_Analysis/myLib/mailer.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+
+class Mailer:
+    
+    def __init__(self):
+        
+        self.EMAIL = "smquadri1999@gmail.com"
+        self.PASS = "cfytwtrlixiipwrn"
+        self.PORT = 465
+        self.server = smtplib.SMTP_SSL('smtp.gmail.com', self.PORT)
+
+    def send(self, mail):
+        
+        self.server = smtplib.SMTP_SSL('smtp.gmail.com', self.PORT)
+        self.server.login(self.EMAIL, self.PASS)
+        
+        SUBJECT = 'ALERT'
+        TEXT = f'ALERT - This mail is to hereby inform you that the maximum limit for people violating social distancing in your store has been exceeded !'
+        message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+
+        self.server.sendmail(self.EMAIL, mail, message)
+        self.server.quit()
